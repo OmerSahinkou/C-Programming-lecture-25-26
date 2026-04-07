d6fcc59c75a11984ff17e0c8d9d83f65f7220aae
Ömer Şahin <mr.shn.0@outlook.com>
2026-04-07 16:27:43 +0300

    added lsat question

--- 6.9.c
+++ 6.9.c
@@ -1,8 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(){
 
+void revise(char *p)
+{
+    int x;
+    for(x=0;*(p+x)!=NULL;x++);
+    for(;*(++p)!=NULL;*p=*(p+x),x--);
+}
+
+int main(){
+char str[]="The legend of Sleepy Hallow";
+revise(str);    printf("%s",str);
 printf("\n");   
 return 0;
 }
